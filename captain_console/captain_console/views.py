b4b4d4a5918406a6dea10d18a3ff3103cde6152b
Pálína Kröyer <palinakroyer@Palinas-MacBook-Pro-2.local>
2020-05-09 20:20:35 +0000

    homepage

--- captain_console/captain_console/views.py
+++ captain_console/captain_console/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from product.models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'homepage/index.html')
+    return render(request, 'homepage/index.html',context={'products': Product.objects.all()} )
