d2e8c05ed759ec19573776258818b597804113a2
hildurmargret <hildurg17@ru.is>
2020-05-11 16:46:09 +0000

    laga sma i user/models og user/views og captain_console/urls

--- captain_console/user/models.py
+++ captain_console/user/models.py
@@ -1,26 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import models
-
-
-# Create your models here.
-#class CustomUser(User):
-    #fields = User.fields + ('first_name', 'last_name', 'email')
 from django.db.models.signals import post_save
-
 from product.models import Product
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.CharField(max_length=9999, default='https://img.icons8.com/pastel-glyph/512/000000/user-male--v1.png')
-
     def create_profile(sender, **kwargs):
         user = kwargs["instance"]
         if kwargs["created"]:
             user_profile = Profile(user=user)
             user_profile.save()
-
     post_save.connect(create_profile, sender=User)
 
 
