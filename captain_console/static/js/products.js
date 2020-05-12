9ecd6f3c3c737172d38ac81d48af29ac3faab462
Pálína Kröyer <palinakroyer@Palinas-MacBook-Pro-2.local>
2020-05-12 10:27:07 +0000

    aerjæga

--- captain_console/static/js/products.js
+++ captain_console/static/js/products.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
                 let newHtml = resp.data.map(d => {
                     if (d.type_id === 1) {
-                        return `<div class="well products">
+                        return `<div class="well product">
                             <a href="/products/games/${d.id}">
                                 <img class="product-img" src="${d.firstImage}"/>
                                 <h4>${d.name}</h4>
